b764420d30c994434c801e84a7c5f1c7269ae0d4
Drakers <drakers@naver.com>
2025-07-08 18:18:10 +0900

    feat: Chart내의 ChartPane들의 높이 조절
    - 각 ChartPane의 하단부를 드래그 하여 높이 조절
    - SetChartPaneHeightRatio 메소드를 이용하여 코드로 각 ChartPane의 높이 비율 설정

---
+++ src/DrakersChart/Chart.cs
@@ -0,0 +1,181 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DrakersChart;
+public sealed class Chart : UserControl
+{
+    private readonly Grid mainGrid = new();
+    private readonly Canvas canvas = new();
+    private readonly List<ChartPane> chartList = [];
+    private readonly List<Double> ratioList = [];
+    private readonly CrosshairLayer crosshairLayer = new();
+    private readonly SizeGripLayer gripLayer;
+
+    public ChartPane[] ChartPanes => this.chartList.ToArray();
+
+    public Chart()
+    {
+        this.gripLayer = new SizeGripLayer(this, this.crosshairLayer);
+        AddChild(this.mainGrid);
+        this.mainGrid.Children.Add(this.canvas);
+        this.mainGrid.Children.Add(this.crosshairLayer);
+        this.mainGrid.Children.Add(this.gripLayer);
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        foreach (var eachChart in this.chartList)
+        {
+            eachChart.Width = availableSize.Width;
+        }
+
+        SetChartPaneHeight(availableSize.Height);
+        this.gripLayer.UpdateGripArea();
+        return base.MeasureOverride(availableSize);
+    }
+
+    /// <summary>
+    /// 각 ChartPane의 높이를 비율대로 설정한다
+    /// </summary>
+    /// <param name="totalHeight">전체 Height</param>
+    private void SetChartPaneHeight(Double totalHeight)
+    {
+        if (this.chartList.Count > 0 && this.ratioList.Count == 0)
+        {
+            this.ratioList.Clear();
+            Double ratio = 1 / (Double)this.ratioList.Count;
+            for (Int32 index = 0; index < this.chartList.Count; index++)
+            {
+                this.ratioList.Add(ratio);
+            }
+        }
+
+        if (this.chartList.Count == 0)
+        {
+            return;
+        }
+
+        Double sumHeight = 0;
+        for (Int32 index = 0; index < this.chartList.Count - 1; index++)
+        {
+            this.chartList[index].Height = (Int32)(totalHeight * this.ratioList[index]);
+            Canvas.SetTop(this.chartList[index], sumHeight);
+            sumHeight += this.chartList[index].Height;
+        }
+
+        Canvas.SetTop(this.chartList[^1], sumHeight);
+        this.chartList[^1].Height = totalHeight - sumHeight;
+    }
+
+    /// <summary>
+    /// 현재 ChartPane높이에 따른 비율을 업데이트
+    /// </summary>
+    public void UpdateChartPaneRatio()
+    {
+        Double totalHeight = this.chartList.Sum(chart => chart.Height);
+        this.ratioList.Clear();
+        foreach (var eachPane in this.chartList)
+        {
+            this.ratioList.Add(eachPane.Height / totalHeight);
+        }
+    }
+
+    /// <summary>
+    /// 입력된 비율대로 각 ChartPane의 Height를 설정
+    /// </summary>
+    /// <param name="ratios">비율들</param>
+    public void SetChartPaneHeightRatio(Double[] ratios)
+    {
+        if (ratios.Length == 0)
+        {
+            return;
+        }
+        
+        if (this.chartList.Count != ratios.Length)
+        {
+            throw new ApplicationException("비율의 수는 ChartPane의 수와 같아야 합니다");
+        }
+        
+        Double total = 0;
+        for (Int32 index = 0; index < ratios.Length - 1; index++)
+        {
+            total += ratios[index];
+        }
+
+        if (total >= 1)
+        {
+            throw new ApplicationException("전체 비율이 1 이상 입니다");
+        }
+        
+        this.ratioList.Clear();
+        this.ratioList.AddRange(ratios);
+        if (!Double.IsNaN(this.Height))
+        {
+            SetChartPaneHeight(this.Height);
+        }
+    }
+
+    public ChartPane AddChart()
+    {
+        var newChart = new ChartPane();
+
+        if (this.chartList.Count == 0)
+        {
+            this.ratioList.Add(1);
+        }
+        else
+        {
+            Double totalRatio = this.ratioList.Sum();
+            Double avg = totalRatio / this.ratioList.Count;
+            Double ratio = totalRatio / (totalRatio + avg);
+
+            for (Int32 index = 0; index < this.ratioList.Count; index++)
+            {
+                this.ratioList[index] *= ratio;
+            }
+
+            this.ratioList.Add(1 - this.ratioList.Sum());
+        }
+
+        Canvas.SetLeft(newChart, 0);
+        Canvas.SetTop(newChart, 0);
+        this.chartList.Add(newChart);
+        this.canvas.Children.Add(newChart);
+        return newChart;
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        var pos = e.GetPosition(this.crosshairLayer);
+        this.crosshairLayer.UpdatePosition(pos);
+        this.gripLayer.UpdatePosition(pos);
+        base.OnMouseMove(e);
+    }
+
+    protected override void OnMouseLeave(MouseEventArgs e)
+    {
+        this.crosshairLayer.HideCrosshair();
+        this.gripLayer.CancelSizeGrip();
+        base.OnMouseLeave(e);
+    }
+
+    protected override void OnMouseDown(MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            this.gripLayer.LeftMouseDown();
+        }
+
+        base.OnMouseDown(e);
+    }
+
+    protected override void OnMouseUp(MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Released)
+        {
+            this.gripLayer.LeftMouseUp();
+        }
+        base.OnMouseUp(e);
+    }
+}
---
+++ src/DrakersChart/ChartPane.cs
@@ -0,0 +1,60 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using SkiaSharp.Views.WPF;
+using Size = System.Windows.Size;
+
+namespace DrakersChart;
+public class ChartPane : SKGLElement
+{
+    private SKBitmap staticLayer = new();
+
+    #region Draw Flag
+
+    private Boolean reDrawAll;
+
+    #endregion
+    
+    public SKColor BackgroundColor { get; set; } = SKColors.White;
+    
+    public ChartPane()
+    {
+        this.PaintSurface += OnPaintSurface;
+    }
+
+    #region Draw
+
+    private void OnPaintSurface(Object? sender, SKPaintGLSurfaceEventArgs e)
+    {
+        
+        DrawChart(e.Surface.Canvas);
+    }
+    
+    private void DrawChart(SKCanvas canvas)
+    {
+        InitializeStaticLayer(canvas);
+        canvas.Clear(this.BackgroundColor);
+        
+        canvas.DrawBitmap(this.staticLayer, 0, 0);
+    }
+
+    private void InitializeStaticLayer(SKCanvas canvas)
+    {
+        if (!this.reDrawAll)
+        {
+            return;
+        }
+
+        var bounds = canvas.LocalClipBounds;
+        Int32 width  = (Int32)bounds.Width;
+        Int32 height = (Int32)bounds.Height;
+        this.staticLayer = new SKBitmap(width, height);
+    }
+
+    #endregion
+    
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        this.reDrawAll = true;
+        return base.MeasureOverride(availableSize);
+    }
+}
